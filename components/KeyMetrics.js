474517d2966c5a963e8277a80036bf9a398f6e95
Casey <leptone55@gmail.com>
2022-06-03 21:27:38 -0700

    Key Metrics

--- components/KeyMetrics.js
+++ components/KeyMetrics.js
@@ -3,15 +3,25 @@ export default function KeyMetric() {
   return (
     <Container>
       <h2>Key Metrics</h2>
+      <p>Across Sound.xyz, Catalog and Mint Songs</p>
       <Grid container spacing={2}>
         <Grid item xs={4}>
-          <Card>hello</Card>
+          <Card>
+            <h3>Total Primary Sales</h3>
+            <p>###</p>
+          </Card>
         </Grid>
         <Grid item xs={4}>
-          <Card>hello</Card>
+          <Card>
+            <h3>Total Secondary Sales</h3>
+            <p>###</p>
+          </Card>
         </Grid>
         <Grid item xs={4}>
-          <Card>hello</Card>
+          <Card>
+            <h3>No. Unique Collectors</h3>
+            <p>###</p>
+          </Card>
         </Grid>
       </Grid>
     </Container>
