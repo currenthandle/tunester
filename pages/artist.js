6bd1b20dcaa126616c4252172f86ff69a94f495f
Casey <leptone55@gmail.com>
2022-06-04 16:56:15 -0700

    added override attempt

--- pages/artist.js
+++ pages/artist.js
@@ -36,7 +36,7 @@ export default function artist() {
   return (
     <Grid className='w-[100vw] h-full p-0'>
       <Grid item xs={12}>
-        <Container className='m-0 p-0'>
+        <Container className='m-0 p-0 !max-w-none'>
           <Banner name={artist?.name} />
           <Container className='m-0 p-0 bg-[#F5F5F5] h-full'>
             <KeyMetrics
