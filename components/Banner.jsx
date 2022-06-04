c0cf4bb3f93bed763f696dd24847944f3ac41177
Casey <leptone55@gmail.com>
2022-06-04 13:19:54 -0700

    not working state

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -1,20 +1,21 @@
 import { Container, Card } from '@mui/material';
 import Image from 'next/image';
 
-export default function Banner() {
+export default function Banner(props) {
+  console.log('props', props);
   return (
     <Container>
-      <div className='flex flex-col h-[35vh] '>
-        <Card className='absolute top-0 -z-10 h-[35vh] w-[50vw]'>
+      <div className='h-[35vh] '>
+        <Card className='h-[35vh] w-[50vw] relative'>
+          <h1 className='absolute text-2xl font-bold lg:text-7xl bottom-0 z-10 text-red-700'>
+            {props.name}
+          </h1>
           <Image
             src='https://hotfm.ng/lagos/wp-content/uploads/sites/2/2021/08/Nirvana_1_1_1603883064Muzooka.jpg'
             alt='nirvana'
             layout='fill'
             objectFit='cover'
           />
-          <h1 className='text-2xl font-bold lg:text-7xl bottom-0 z-10 text-red-700'>
-            NIRVANA
-          </h1>
         </Card>
       </div>
     </Container>
--- pages/artist.js
+++ pages/artist.js
@@ -4,29 +4,41 @@ import KeyMetrics from '../components/KeyMetrics';
 import Discography from '../components/Discography';
 import Followers from '../components/Followers';
 import Listenership from '../components/Listenership';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import { loadArtist, loadArtists } from '../requests';
 
 export default function artist() {
+  const [artist, setArtist] = useState(null);
+
   useEffect(() => {
     const id = 'Artist:d35489e0-dcb3-40dd-8bc7-5b0586bfa5e8';
+    console.log('initializing artist', artist);
+
     const callLoadArtist = async (id) => {
       const artist = await loadArtist(id);
       console.log('artist', artist);
+      return artist;
     };
-    callLoadArtist(id);
+    if (artist) {
+      console.log('artist', artist);
+    } else {
+      callLoadArtist(id);
+      setArtist(artist);
+      console.log('artist not loaded');
+    }
 
     const callLoadArtists = async () => {
       const artists = await loadArtists();
       console.log('artists', artists);
     };
     //callLoadArtists();
-  });
+  }, [artist]);
+  console.log('state', artist);
 
   return (
     <Container maxWidth='md' style={{ height: '30%' }}>
-      <Banner />
+      <Banner name={artist?.name} />
       <KeyMetrics />
       <Discography />
       <Followers />
