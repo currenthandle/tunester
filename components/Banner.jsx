90dc73b690104b6e75b02720b91f56d9afb33f32
Casey <leptone55@gmail.com>
2022-06-04 22:00:25 -0700

    did some shit to Banner

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -5,10 +5,16 @@ export default function Banner({ name, imgUrl }) {
   const fLCapital = (s) => s?.replace(/./, (c) => c.toUpperCase());
   return (
     <Container className='h-[20vh] relative m-0 max-w-none w-[100vw]'>
-      {name && (
+      {name ? (
         <h1 className='absolute text-2xl font-bold lg:text-7xl bottom-5 left-7 z-10 text-white'>
           {fLCapital(name)}
         </h1>
+      ) : (
+        <Container className='flex flex-col justify-center'>
+          <Card className='bg-white z-10 relative max-w-fit p-3 bg-opacity-40'>
+            <Image src='/tunester.svg' alt='Tunster' width={500} height={100} />
+          </Card>
+        </Container>
       )}
       <Image src={imgUrl} alt='nirvana' layout='fill' objectFit='cover' />
     </Container>
