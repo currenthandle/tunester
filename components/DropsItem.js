796c5f661227c7bdb9723ed04ddca0f9202534e1
Casey <leptone55@gmail.com>
2022-06-05 03:30:53 -0700

    padding

--- components/DropsItem.js
+++ components/DropsItem.js
@@ -4,16 +4,15 @@ export default function DropsItem({ drop }) {
   return (
     <Grid item xs={3}>
       <Link className='no-underline ' href={`/artist/${drop?.id.slice(7)}`}>
-        <Box className='bg-white p-3 flex flex-col rounded-md'>
+        <Box className='bg-white px-3 pt-5 pb-4 flex flex-col rounded-md'>
           <div className='flex justify-center'>
-            <Box className='bg-[#0D6EBB] flex content-center	p-1.5 rounded-md bg-opacity-20'>
-              <Image
-                alt='album'
-                src={drop['artist_name_image_relation']?.image}
-                height={200}
-                width={200}
-              />
-            </Box>
+            <Image
+              alt='album'
+              src={drop['artist_name_image_relation']?.image}
+              height={200}
+              width={200}
+              className='rounded'
+            />
           </div>
           <Box className='px-4'>
             <h3 className='text-black font-bold pt-4 text-[20px]'>
