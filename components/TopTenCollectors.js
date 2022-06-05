e99893e45423570e2978b40ef211ef27ddbc374b
Casey <leptone55@gmail.com>
2022-06-04 19:27:47 -0700

    styling-table

--- components/TopTenCollectors.js
+++ components/TopTenCollectors.js
@@ -27,12 +27,22 @@ export default function TopTenCollectors() {
       <TableContainer component={Paper} className='p-4'>
         <Table sx={{ minWidth: 650 }} aria-label='simple table'>
           <TableHead className='bg-[#DADADA]'>
-            <TableRow>
-              <TableCell>Dessert (100g serving)</TableCell>
-              <TableCell align='right'>Calories</TableCell>
-              <TableCell align='right'>Fat&nbsp;(g)</TableCell>
-              <TableCell align='right'>Carbs&nbsp;(g)</TableCell>
-              <TableCell align='right'>Protein&nbsp;(g)</TableCell>
+            <TableRow className=''>
+              <TableCell className='border-none rounded-l-lg'>
+                Dessert (100g serving)
+              </TableCell>
+              <TableCell className='border-none' align='right'>
+                Calories
+              </TableCell>
+              <TableCell className='border-none' align='right'>
+                Fat
+              </TableCell>
+              <TableCell className='border-none' align='right'>
+                Carbs
+              </TableCell>
+              <TableCell className='border-none' align='right'>
+                Protein
+              </TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
