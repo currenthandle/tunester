4aeee829847adebb01564e4391c2b8f5903f7b10
Casey <leptone55@gmail.com>
2022-06-05 12:53:32 -0700

    new purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.js',
+    './components/**/*.js',
   ],
   theme: {
     extend: {},
